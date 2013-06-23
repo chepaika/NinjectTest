83ea7f8c0467ca65e954170dbcd21bb07f77554e
chepaika <chepaika@gmail.com>
2013-06-23 21:10:58 +0400

    Add class ShoppingCatr

--- NinjectDemo/Product.cs
+++ NinjectDemo/Product.cs
@@ -9,6 +9,22 @@ namespace NinjectDemo
     class Product
     {
         public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
 
+    }
+
+    public interface IValueCalculator
+    {
+        decimal ValueProducts(params Product[] products);
+    }
+    public class LinqValueCalculator : IValueCalculator
+    {
+        public decimal ValueProducts(params Product[] products)
+        {
+            return products.Sum(p => p.Price);
+        }
     }
 }
---
+++ NinjectDemo/ShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjectDemo
+{
+    class ShoppingCart
+    {
+        private IValueCalculator calculator;
+        public ShoppingCart(IValueCalculator calcParam)
+        {
+            calculator = calcParam;
+        }
+
+        public decimal CalculateStockValue()
+        {
+            Product[] products = 
+            {
+                new Product() {Name="Kayak", Price=275M},
+                new Product() {Name="Lifejacket", Price=48.95M},
+                new Product() {Name="Soccer ball", Price=19.50M}
+            };
+
+            decimal totalValue = calculator.ValueProducts(products);
+
+            return totalValue;
+        }
+    }
+}
